8fc8df16e8d223f7a78c5c85085a418eabef3386
Vladimir Semikov <voesem@ya.ru>
2023-06-04 15:36:22 +0300

    fixture in test_dicts.py added

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -4,13 +4,15 @@ import pytest
 from utils.dicts import get_val
 
 
-data = {'name': 'Vladimir', 'surname': 'Semikov', 'age': 27}
+@pytest.fixture()
+def data_fixture():
+    return {'name': 'Vladimir', 'surname': 'Semikov', 'age': 27}
 
 
-def test_get_val():
-    assert get_val(data, 'surname') == 'Semikov'
-    assert get_val(data, 'name') == 'Vladimir'
-    assert get_val(data, 'age') == 27
-    assert get_val(data, 'education') == 'git'
-    assert get_val(data, 'education', 'not found') == 'not found'
+def test_get_val(data_fixture):
+    assert get_val(data_fixture, 'surname') == 'Semikov'
+    assert get_val(data_fixture, 'name') == 'Vladimir'
+    assert get_val(data_fixture, 'age') == 27
+    assert get_val(data_fixture, 'education') == 'git'
+    assert get_val(data_fixture, 'education', 'not found') == 'not found'
     assert get_val('abcdefg', 'name') == 'git'
